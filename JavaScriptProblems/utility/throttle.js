4e044673e853d7439f8ef4ca725bef833854567f
Arghya Majumder <shyamalm88@gmail.com>
2026-02-02 03:34:05 +0530

    Update throttle.js

--- JavaScriptProblems/utility/throttle.js
+++ JavaScriptProblems/utility/throttle.js
@@ -1,10 +1,19 @@
 function throttle(fn, delay) {
   let lastRun = 0;
+  let timer = null;
+
   return function (...args) {
     const now = Date.now();
+
     if (now - lastRun >= delay) {
       fn.apply(this, args);
       lastRun = now;
+    } else if (!timer) {
+      timer = setTimeout(() => {
+        fn.apply(this, args);
+        lastRun = Date.now();
+        timer = null;
+      }, delay - (now - lastRun));
     }
   };
 }
